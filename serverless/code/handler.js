4f8e5eea7efcafb4dfe70e51147ba8aaa065501e
relliott57 <72311877+relliott57@users.noreply.github.com>
2024-06-05 22:25:48 -0500

    Update handler.js

--- serverless/code/handler.js
+++ serverless/code/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v2.0! Your function er executed successfully!",
+        message: "Go Serverless v2.0! Your function er has executed successfully!",
         input: event,
       },
       null,
